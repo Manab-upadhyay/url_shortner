1c00c4de571d37490d236e93be6eab7252a19a0b
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2026-02-20 23:59:58 +0530

    feat: added type cheker using zod

--- src/modules/link/link.route.ts
+++ src/modules/link/link.route.ts
@@ -6,9 +6,19 @@ import {
 } from "./link.controller";
 import { Router } from "express";
 import { protect } from "../../middleware/auth.middleware";
+import { validate } from "../../middleware/validate.middlewere";
+import {
+  createLinkSchema,
+  redirectSchema,
+} from "../../validator/link.validator";
 const router = Router();
-router.post("/addLink", protect, addLinkController);
-router.get("/getLink/:shortCode", protect, getLinkController);
+router.post("/addLink", protect, validate(createLinkSchema), addLinkController);
+router.get(
+  "/getLink/:shortCode",
+  protect,
+  validate(redirectSchema),
+  getLinkController,
+);
 router.get("/getUserLinks", protect, getUserLinksController);
 router.delete("/deleteLink/:linkId", protect, deleteLinkController);
 export default router;
--- src/validator/link.validator.ts
+++ src/validator/link.validator.ts
@@ -4,6 +4,7 @@ export const createLinkSchema = z.object({
   body: z.object({
     url: z.string().url("Invalid URL format"),
     customAlias: z.string().min(3).max(20).optional(),
+    name: z.string().min(1, "Name is required"),
   }),
 });
 
